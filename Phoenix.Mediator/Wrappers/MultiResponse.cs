f88d94839453a5a95ffcced8c3efa2654380d13e
Phoenix Company <lunar.cry95@gmail.com>
2026-01-04 18:10:30 +0300

    Modified the multiresponse wrapper

--- Phoenix.Mediator/Wrappers/MultiResponse.cs
+++ Phoenix.Mediator/Wrappers/MultiResponse.cs
@@ -1,8 +1,10 @@
 namespace Phoenix.Mediator.Wrappers;
 
-public class MultiResponse<T>(List<T> data,int pagesCount, int totalCount)
+public class MultiResponse<T>(List<T> data, int totalCount, int pageSize)
 {
     public List<T> Data => data;
-    public int PagesCount => pagesCount;
     public int TotalCount => totalCount;
+    public int PagesCount => pageSize == 0
+        ? 0
+        : (int)Math.Ceiling((double)totalCount / pageSize);
 }
